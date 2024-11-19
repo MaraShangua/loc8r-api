58bab3de606b1a2634296f6efdd5f13dae9bb78a
Mara <153260407+MaraShangua@users.noreply.github.com>
2024-11-19 17:38:19 +0900

    Add files via upload

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -4,7 +4,7 @@ const apiOptions = {
   server: 'http://localhost:3000'
 };
 if(process.env.NODE_ENV === 'production'){
-  apiOptions.server  = 'https://YOURAPI.onrender.com';
+  apiOptions.server  = 'https://loc8r-api-t9kv.onrender.com';
 }
 
 const homelist = (req, res) => {
